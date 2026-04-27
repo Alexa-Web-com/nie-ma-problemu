f3660da9edfbd26fb10544cba79de59bdda79198
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-04-27 22:20:09 +0200

    quick fix

--- src/pages/OgladajGaleria1.tsx
+++ src/pages/OgladajGaleria1.tsx
@@ -1,23 +1,21 @@
-import ImageGallery from '@/components/ImageGallery'
+import ImageGallery from "@/components/ImageGallery";
 
 import doorsHidden from "@/assets/doors-hidden.jpg";
 import doorsClassic from "@/assets/doors-classic.jpg";
 import doorsSliding from "@/assets/doors-sliding.jpg";
 import doorsGlass from "@/assets/doors-glass.jpg";
-import doorsHandle from "@/assets/doors-handle.png";
+import doorsHandle from "@/assets/doors-handle.jpg";
 
 const images: string[] = [
-    doorsHidden,
-    doorsClassic,
-    doorsSliding,
-    doorsGlass,
-    doorsHandle,
+  doorsHidden,
+  doorsClassic,
+  doorsSliding,
+  doorsGlass,
+  doorsHandle,
 ];
 
 const OgladajGaleria1 = () => {
-  return (
-    <ImageGallery images={images} />
-  )
-}
+  return <ImageGallery images={images} />;
+};
 
-export default OgladajGaleria1
+export default OgladajGaleria1;
