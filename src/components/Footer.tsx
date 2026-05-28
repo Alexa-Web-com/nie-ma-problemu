5b72179ca41ec87d885ac524d4a4b3ece0de9861
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-05-28 14:54:24 +0200

    youtube link update, caption fonts resize, button color change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,7 +80,7 @@ const Footer = () => {
                 <Instagram className="w-5 h-5" />
               </a>
               <a
-                href="https://youtube.com"
+                href="https://youtube.com/@niemaproblemugdynia"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 rounded-full bg-footer-foreground/10 flex items-center justify-center hover:bg-accent hover:text-footer transition-colors"
